87c2f4b6ef8a03758fc4ffd9cf3414f02dc52366
Christophe Antoniewski <drosofff@gmail.com>
2023-01-19 15:55:50 +0000

    a running docker container !

--- docker_startup.sh
+++ docker_startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
-/etc/init.d/postgresql start
-/etc/init.d/nginx start
-/home/galaxy/galaxy/.venv/bin/galaxyctl reload
+ /etc/init.d/postgresql start && \
+source /home/galaxy/galaxy/.venv/bin/activate && \
+/home/galaxy/galaxy/.venv/bin/galaxyctl start && \
+/etc/init.d/nginx start && \
+while true; do sleep 1; done
